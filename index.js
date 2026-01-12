dbe0933eb157fa07772b25083ebb047e194e81f8
aldofranklin <aldofranklin@gmail.com>
2026-01-12 14:13:08 -0300

    passo segunda

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
-console.log("olá mundo")
-let variavel = 0
+let nome = "Aldo";
+let XP = 2000;
+
+switch (true) {
+  case XP < 1000:
+    console.log("Ferro");
+    break;
+
+  case XP >= 1000 && XP < 2001:
+    console.log("Bronze");
+    break;
+
+  case XP >= 2001 && XP < 5001:
+    console.log("Prata");
+    break;
+
+  case XP >= 5001 && XP < 7001:
+    console.log("Ouro");
+    break;
+
+  case XP >= 7001 && XP < 8001:
+    console.log("Platina");
+    break;
+
+  default:
+    console.log("Imortal"); // ou o que você quiser para XP >= 8001
+    break;
+}
+
+console.log(`O herói ${nome} tem ${XP} de XP.`);
