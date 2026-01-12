4195b7f37a3ccbd48b92747bb7712bc61f74023c
aldofranklin <aldofranklin@gmail.com>
2026-01-12 20:09:11 -0300

    corrigido

--- classificador_heroi.js
+++ classificador_heroi.js
@@ -4,28 +4,29 @@ let XP = 5000;
 
 switch (true) {
   case XP < 1000:
-    console.log("Ferro");
+    console.log("Nível: Ferro");
     break;
 
   case XP >= 1000 && XP < 2001:
-    console.log("Bronze");
+    console.log("Nível: Bronze");
     break;
 
   case XP >= 2001 && XP < 5001:
-    console.log("Prata");
+    console.log("Nível: Prata");
     break;
 
   case XP >= 5001 && XP < 7001:
-    console.log("Ouro");
+    console.log("Nível: Ouro");
     break;
 
   case XP >= 7001 && XP < 8001:
-    console.log("Platina");
+    console.log("Nível: Platina");
     break;
 
   default:
-    console.log("Imortal"); 
+    console.log("Nível: Imortal"); 
     break;
 }
 
 console.log("O herói " + nome + " tem " + XP + " de XP.");
+
